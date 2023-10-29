5f9a3e60ebaff175fd57dfe34a37aa233aa4a829
palashb01 <palash.baderia@outlook.com>
2023-10-29 19:02:58 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      boxShadow: {
+        '3xl': '2px 2px 30px 10px rgba(0, 0, 0, 0.20)',
+      },
+      colors: {
+        lightgray: 'rgba(245, 245, 245, 0.50)',
+      },
+      fontFamily: {
+        gt: ['var(--font-gt)'],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
