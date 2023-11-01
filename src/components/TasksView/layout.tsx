ffa5ee4d4a89c791a4834b7a5bcef098ce6de9a3
palashb01 <palash.baderia@outlook.com>
2023-11-01 17:44:59 +0530

    fix the key prop error

--- src/components/TasksView/layout.tsx
+++ src/components/TasksView/layout.tsx
@@ -13,8 +13,8 @@ const Layout: React.FC<LayoutProps> = ({ tasks }) => {
   return (
     <div className='flex w-2/5 flex-col gap-16 py-4 max-[800px]:w-2/3'>
       {tasks.map((task) => (
-        <div className='border-1 flex flex-col rounded-[10px] shadow-3xl '>
-          <div key={task.id} className='px-3 py-3'>
+        <div key={task.id} className='border-1 flex flex-col rounded-[10px] shadow-3xl '>
+          <div className='px-3 py-3'>
             <TaskBarTitle />
             <div className='px-2'>
               <h1 className='text-md py-2 text-[#6C6F75]'>{task.taskTitle}</h1>
